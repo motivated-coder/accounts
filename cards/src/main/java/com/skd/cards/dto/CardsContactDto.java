21f8165f330bb9686235e79fd4b4ac5438673c3d
motivated-coder <sumeetdwivedi11@outlook.com>
2024-02-02 00:45:37 +0530

    updated configuration to use git

--- cards/src/main/java/com/skd/cards/dto/CardsContactDto.java
+++ cards/src/main/java/com/skd/cards/dto/CardsContactDto.java
@@ -1,10 +1,17 @@
 package com.skd.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactDto(String message, Map<String,String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardsContactDto {
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
 }
